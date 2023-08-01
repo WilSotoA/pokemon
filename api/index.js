1bc132630330347e229ec48605a44ab7aedfd6fc
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-08-01 11:53:51 -0500

    add config port

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+const { NODE_PORT } = process.env || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(NODE_PORT, () => {
+    console.log(`Listening in port ${NODE_PORT}`); // eslint-disable-line no-console
   });
 });
